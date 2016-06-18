c0c299cd4a605b97f2f62d3463c4113ded937b7b
Aarni Koskela <akx@iki.fi>
2016-06-18 17:36:20 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "airbnb-base",
+  "rules": {
+    "object-curly-spacing": ["error", "never"],
+    "no-console": ["off"],
+    "comma-dangle": ["error", "only-multiline"],
+  }
+};
---
+++ index.js
@@ -0,0 +1,48 @@
+const config = require('./config');
+const knex = require('knex')(config.knex);
+const express = require('express');
+const shortid = require('shortid');
+const Promise = require('bluebird');
+const makeSchema = require('./lib/makeSchema');
+
+const app = express();
+
+app.all('/track', ({ip, query}, res) => {
+  const ts = (+new Date());
+  const req = shortid.generate();
+  const node = (query.node || '').toLowerCase();
+  if (!node) {
+    res.status(400).send('need node param');
+    return;
+  }
+  const measures = Object.keys(query).map((key) => {
+    if (key === 'node') return null;
+    const value = parseFloat(query[key]);
+    if (!isNaN(value)) {
+      return {key, value};
+    }
+    return null;
+  }).filter((m) => !!m);
+  if (!measures.length) {
+    res.status(400).send('no valid measures');
+    return;
+  }
+  knex.transaction((trx) =>
+    trx.insert({ts, node, req, data: JSON.stringify({ts, ip, query})}).into('request')
+    .then(() => Promise.map(
+      measures,
+      ({key, value}) => trx.insert({ts, node, req, key, value}).into('measure'))
+    )
+  ).then((measureInserts) => {
+    res.send(`ok ${measureInserts.length}`);
+  }).catch((err) => {
+    console.log(err);
+    res.status(400).send(err);
+  });
+});
+
+
+makeSchema(knex).then(() => {
+  app.listen(33333);
+});
+

b50b612c0bced2015af9d8b496db9b77ff973dcc
Aarni Koskela <akx@iki.fi>
2017-10-24 14:25:03 +0300

    Get rid of config.json

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const config = require('./config');
-const knex = require('knex')(config.knex);
+const Knex = require('knex');
 const makeSchema = require('./lib/makeSchema');
-const app = require('./lib/app')({ knex });
+const App = require('./lib/app');
 
 const PORT = (parseInt(process.env.PORT, 10) || 33333);
 const HOST = process.env.HOST || null;
+const KNEX_CONFIG = JSON.parse(process.env.KNEX_CONFIG || `{
+  "client": "sqlite3",
+  "connection": {
+    "filename": "./iotlog.sqlite"
+  },
+  "useNullAsDefault": true
+}`);
 
+const knex = Knex(KNEX_CONFIG);
+const app = App({ knex });
 
 makeSchema(knex).then(() => {
   app.listen(PORT, HOST, (err) => {
